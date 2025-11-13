24483a19cff9d07df0d5b102d7187a788ed96563
Alla Pavlova <Pavlo_fo95@student.itstep.org>
2025-11-13 00:19:45 +0000

    fix: use t4g.micro for arm64 AMI

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -226,7 +226,7 @@ variable "db_engine_version" {
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
-  default     = "db.t3.micro"
+  default     = "db.t4g.micro"
 }
 
 variable "db_storage_size" {
